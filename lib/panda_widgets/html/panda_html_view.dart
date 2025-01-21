170f67706ddedc5261c2535a5aa602115e568a73
hafiz-panda-software <80079662+hafiz-panda-software@users.noreply.github.com>
2025-01-21 17:10:14 +0800

    0.0.20 - Remove table border and header color

--- lib/panda_widgets/html/panda_html_view.dart
+++ lib/panda_widgets/html/panda_html_view.dart
@@ -47,8 +47,6 @@ class PandaHtmlView extends StatelessWidget {
             'text-align': 'center',
             'vertical-align': 'center',
             //'align-items': 'center',
-            'color': 'blue',
-            'border': '1px solid #B0B0B0	',
             'height': '100px',
           }.entries);
         }
@@ -57,7 +55,6 @@ class PandaHtmlView extends StatelessWidget {
             'text-align': 'start',
             //'vertical-align': 'center',
             //'horizontal-align': 'center',
-            'border': '1px solid #B0B0B0',
             'height': '100px',
           }.entries);
         }
